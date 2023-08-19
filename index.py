ef34e8991c5f8fef486b64d62531a3044e31feb4
Yuqiao0428 <leah.huang0428@gmail.com>
2023-08-19 23:05:13 +0800

    flask

--- index.py
+++ index.py
@@ -1,12 +1,25 @@
-import train_2
-import dash
-import dash_html_components as html
+'''
+    import train_2
+    import dash
+    import dash_html_components as html
 
-app = dash.Dash(__name__)
+    app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    html.H1('Hello python page...')
-)
+    app.layout = html.Div(
+        html.H1('Hello python page...')
+    )
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
+    if __name__ == '__main__':
+        app.run_server(debug=False)
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
